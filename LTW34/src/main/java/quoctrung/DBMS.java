253891128ca5b3009bc731f6ecf89cb55974d761
junUTE <vuquoctrung0@gmail.com>
2025-08-27 12:47:24 +0700

    kết nối CSDL

---
+++ LTW34/src/main/java/quoctrung/DBMS.java
@@ -0,0 +1,64 @@
+package quoctrung;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBMS {
+    private final String serverName = "localhost"; // hoặc IP
+    private final String dbName = "Chuong2";
+    private final String portNumber = "1433";
+    private final String instance = ""; // nếu có instance: "SQLEXPRESS"
+    private final String userID = "sa";
+    private final String password = "1";
+
+    public Connection getConnection() throws Exception {
+        String url;
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber
+                + ";databaseName=" + dbName
+                + ";encrypt=false;trustServerCertificate=true";
+        } else {
+            url = "jdbc:sqlserver://" + serverName + "\\" + instance + ":" + portNumber
+                + ";databaseName=" + dbName
+                + ";encrypt=false;trustServerCertificate=true";
+        }
+
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+
+    public static void main(String[] args) {
+            String sqlInsert = "INSERT INTO users VALUES(?, ?, ?)";
+            String selectAll = "SELECT * FROM users";
+
+            try {
+                // connect to database
+                Connection conn = new DBMS().getConnection();
+
+                // create statement to insert
+                PreparedStatement stmt = conn.prepareStatement(sqlInsert);
+                stmt.setInt(1, 1);
+                stmt.setString(2, "Trung");
+                stmt.setString(3, "HCM");
+                stmt.execute();
+
+                // select all
+                stmt = conn.prepareStatement(selectAll);
+
+                // get data from table
+                ResultSet rs = stmt.executeQuery();
+
+                // show data
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+                }
+
+                stmt.close();
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+}
